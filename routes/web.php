88224dd5df0e45641447c81a948c00925d840b17
lyperrr <kadekwilly006@gmail.com>
2026-09-12 08:46:50 +0800

    feat: add redirect from /admin/login to /login

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::get('/', function () {
 });
 
 // Authentication Routes
+Route::redirect('/admin/login', '/login');
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
