d7588b19270d755375021cb2451e99ca05e7f123
lyperrr <kadekwilly006@gmail.com>
2026-09-12 16:35:08 +0800

    feat: add favicon and apple-touch-icon to app layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'ARTDEVATA Photography') }}</title>
 
+        <!-- Favicon / Logo -->
+        <link rel="icon" type="image/svg+xml" href="{{ asset('logo.svg') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+        <link rel="apple-touch-icon" href="{{ asset('logo.svg') }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
